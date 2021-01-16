52baef4c2f268da406274e1a2c36a5e439638909
1danil10 <danilmatvienko@gmail.com>
2021-01-16 14:30:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let brackets = {};
+    bracketsConfig.forEach((el) => {
+        brackets[el[0]] = el[1];
+    });
+
+    for (let i = 0; i <= str.length; i++) {
+        let char = str[i];
+        let last = stack[stack.length - 1] || 0;
+
+        if (brackets[char]) {
+            if (last !== brackets[char]) {
+                stack.push(char);
+            } else stack.pop();
+        } else if (brackets[stack.pop()] !== char) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
